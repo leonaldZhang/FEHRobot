10e3c1c5f30289bb268c6cca3cf473d602ce906a
leonaldZhang <zhang.12257@buckeyemail.osu.edu>
2022-03-03 18:22:54 -0500

    Code as of 3/3. Worked perfectly a couple times.

--- Proteus_Project/main.cpp
+++ Proteus_Project/main.cpp
@@ -1,3 +1,4 @@
+ 
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
@@ -9,145 +10,157 @@
 #define CDS_RED_THRESHOLD .5
 #define CDS_UPPER_THRESHOLD 2.0
 #define COUNTS_PER_INCH 34.0
-#define NINETY_DEG_TWENTYSPEED 5.1
+#define NINETY_DEG_TWENTYSPEED 5.2
 #define SERVO_MIN 982
 #define SERVO_MAX 2100
-
+ 
 /* Declarations for encoders, motors, and input pins (all subject to
 change) */
-DigitalEncoder right_encoder(FEHIO::P2_0); 
-DigitalEncoder left_encoder(FEHIO::P3_0); //Left stupid
+DigitalEncoder right_encoder(FEHIO::P2_0);
+DigitalEncoder left_encoder(FEHIO::P3_0); // Left stupid
 FEHMotor right_motor(FEHMotor::Motor0, 9.0);
-FEHMotor left_motor(FEHMotor::Motor3, 9.0); //left taped
+FEHMotor left_motor(FEHMotor::Motor3, 9.0); // left taped
 AnalogInputPin cds(FEHIO::P1_0);
 FEHServo arm_servo(FEHServo::Servo0);
-
+ 
 /* function for turning right. use encoders */
-void turn_right(int percentage, int encoder_counts) { 
+void turn_right(int percentage, int encoder_counts)
+{
     // Reset encoder counts
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
-
+ 
     // Set both motors to desired percent
     left_motor.SetPercent(percentage);
     right_motor.SetPercent(-percentage);
-
+ 
     // Keep running motors while average of left and right encoder is less
     // than encoderCounts
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < encoder_counts);
-
+    while ((left_encoder.Counts() + right_encoder.Counts()) / 2. < encoder_counts)
+        ;
+ 
     // turn off motors
     right_motor.Stop();
     left_motor.Stop();
 }
-
+ 
 /* function for turning left. use encoders */
-void turn_left(int percentage, int encoder_counts) { 
+void turn_left(int percentage, int encoder_counts)
+{
     // Reset encoder counts
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
-
+ 
     // Set both motors to desired percent
     left_motor.SetPercent(-percentage);
     right_motor.SetPercent(percentage);
-
+ 
     // Keep running motors while average of left and right encoder is less
     // than encoderCounts
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < encoder_counts);
-
+    while ((left_encoder.Counts() + right_encoder.Counts()) / 2. < encoder_counts)
+        ;
+ 
     // turn off motors
     right_motor.Stop();
     left_motor.Stop();
 }
-
+ 
 /* function for moving forward. use encoders */
-void move_straight(int percentage, int encoder_counts) { 
+void move_straight(int percentage, int encoder_counts)
+{
     // Reset encoder counts
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
-
+ 
     // Set both motors to desired percent
     left_motor.SetPercent(percentage);
     right_motor.SetPercent(percentage);
-
+ 
     // Keep running motors while average of left and right encoder is less
     // than encoderCounts
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2.0 < encoder_counts);
-    
-    
+    while ((left_encoder.Counts() + right_encoder.Counts()) / 2.0 < encoder_counts)
+        ;
+ 
     // turn off motors
     right_motor.Stop();
     left_motor.Stop();
-
-    
 }
-
-/* function for moving at an angle 
-    direction is based off of quadrants (1 is forward right, 2 forward left, etc.) 
+ 
+/* function for moving at an angle
+    direction is based off of quadrants (1 is forward right, 2 forward left, etc.)
     percentage is motor percentage
     angle is the desired angle of turning as a decimal
     encoder counts for how much to go*/
-void angled_turn(int direction, int percentage, float angle, float inches){
+void angled_turn(int direction, int percentage, float angle, float inches)
+{
     // Initializing distance and angle
-    int encoder_counts = inches*COUNTS_PER_INCH;
-    float turn_radius = angle/100.0;
+    int encoder_counts = inches * COUNTS_PER_INCH;
+    float turn_radius = angle / 100.0;
     // Reset encoder counts
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
     // Set both motors to desired percent based on direction
-    int adjusted_angle = percentage*(1-turn_radius);
-    switch (direction){
-        case 1:
+    int adjusted_angle = percentage * (1 - turn_radius);
+    switch (direction)
+    {
+    case 1:
         left_motor.SetPercent(percentage);
         right_motor.SetPercent(adjusted_angle);
         break;
-        case 2:
+    case 2:
         left_motor.SetPercent(adjusted_angle);
         right_motor.SetPercent(percentage);
         break;
-        case 3:
+    case 3:
         left_motor.SetPercent(-adjusted_angle);
         right_motor.SetPercent(-percentage);
         break;
-        case 4:
+    case 4:
         left_motor.SetPercent(-percentage);
         right_motor.SetPercent(-adjusted_angle);
         break;
-        case 5: 
-        left_motor.SetPercent(-percentage/(1-turn_radius));
-        right_motor.SetPercent(percentage/(1-turn_radius));
+    case 5:
+        left_motor.SetPercent(-percentage / (1 - turn_radius));
+        right_motor.SetPercent(percentage / (1 - turn_radius));
         break;
     }
-
+ 
     // Keep running motors while average of left and right encoder is less
     // than encoderCounts
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2.0 < encoder_counts);
-
+    while ((left_encoder.Counts() + right_encoder.Counts()) / 2.0 < encoder_counts)
+        ;
+ 
     // turn off motors
     right_motor.Stop();
     left_motor.Stop();
     Sleep(.25);
 }
 /* Function for moving based off sensing the start light */
-void start(){
+void start()
+{
     // Display what function is currently running
     LCD.Clear();
     LCD.WriteLine("start");
-
+ 
     // Starting servo at 0 degrees
     arm_servo.SetMin(SERVO_MIN);
     arm_servo.SetMax(SERVO_MAX);
     arm_servo.SetDegree(30.0);
-
+ 
     // While elapsed time is less than 30 seconds.
     float time_now = TimeNow();
     bool started = false;
-    while(TimeNow() - time_now < 30 && !started) {
+    while (TimeNow() - time_now < 30 && !started)
+    {
         // Wait until light goes on
-        while (cds.Value() > CDS_BLUE_THRESHOLD);
-        if((cds.Value() > CDS_RED_THRESHOLD) && cds.Value() < CDS_BLUE_THRESHOLD) {
+        while (cds.Value() > CDS_BLUE_THRESHOLD)
+            ;
+        if ((cds.Value() > CDS_RED_THRESHOLD) && cds.Value() < CDS_BLUE_THRESHOLD)
+        {
             LCD.WriteLine("The start line was blue"); // Display what color is read
-        } else {
+        }
+        else
+        {
             LCD.WriteLine("The start line was red"); // Display what color is read
         }
         started = true;
@@ -156,23 +169,26 @@ void start(){
     LCD.WriteLine("Did not read a light within 30 secs. Starting anyway.");
     // Call any function
 }
-
-void toJuke(){
-    move_straight(20, COUNTS_PER_INCH*13);
-    turn_left(20, COUNTS_PER_INCH*5.1);
-    move_straight(20, COUNTS_PER_INCH*16.8);
-    turn_left(20, COUNTS_PER_INCH*5.3);
+ 
+void toJuke()
+{
+    move_straight(20, COUNTS_PER_INCH * 13);
+    turn_left(20, COUNTS_PER_INCH * 5.1);
+    move_straight(20, COUNTS_PER_INCH * 16.8);
+    turn_left(20, COUNTS_PER_INCH * 5.3);
     move_straight(20, COUNTS_PER_INCH * 0.75);
     Sleep(1.0);
 }
-bool pushButton() {
+bool pushButton()
+{
     // Set boolean to know which button was hit. False is red, true is blue
     bool button = false;
     // Display what function is currently running
     LCD.Clear();
-    LCD.WriteLine("pushButton"); 
-
-    if (cds.Value() < CDS_RED_THRESHOLD) {
+    LCD.WriteLine("pushButton");
+ 
+    if (cds.Value() < CDS_RED_THRESHOLD)
+    {
         LCD.Clear(RED); // Display what color is read
         LCD.WriteLine("Need to hit RED");
         LCD.WriteLine(cds.Value());
@@ -180,7 +196,9 @@ bool pushButton() {
         move_straight(20, COUNTS_PER_INCH * 0.5);
         turn_left(20, COUNTS_PER_INCH * 5);
         move_straight(20, COUNTS_PER_INCH * 5);
-    } else {
+    }
+    else
+    {
         LCD.Clear(BLUE); // Display what color is read
         LCD.WriteLine("Need to hit BLUE");
         LCD.WriteLine(cds.Value());
@@ -192,79 +210,109 @@ bool pushButton() {
     }
     return button;
 }
-
-void toRamp(bool redBlue) {
+ 
+void toRamp(bool redBlue)
+{
     LCD.Clear();
     LCD.WriteLine("toRamp");
-    if (!redBlue) {
+    if (!redBlue)
+    {
         move_straight(-20, COUNTS_PER_INCH * 6);
         turn_right(20, COUNTS_PER_INCH * 5);
         move_straight(-20, COUNTS_PER_INCH * 10);
         turn_left(20, COUNTS_PER_INCH * 5);
         move_straight(-20, COUNTS_PER_INCH * 4);
-    } else {
+    }
+    else
+    {
         move_straight(-20, COUNTS_PER_INCH * 6);
         turn_right(20, COUNTS_PER_INCH * 5);
         move_straight(-20, COUNTS_PER_INCH * 9);
         turn_left(20, COUNTS_PER_INCH * 5);
         move_straight(-20, COUNTS_PER_INCH * 4);
     }
-    
 }
-
-void moveArm() {
+ 
+void moveArm()
+{
     // Enter the min and max servo valeus from the .TouchCalibrate() fxn
     arm_servo.SetMin(SERVO_MIN);
     arm_servo.SetMax(SERVO_MAX);
     float x, y;
-    while (!LCD.Touch(&x, &y)){
-    arm_servo.SetDegree(180.0);
+    while (!LCD.Touch(&x, &y))
+    {
+        arm_servo.SetDegree(180.0);
     }
     Sleep(.1);
-    while (LCD.Touch(&x, &y)){
-        arm_servo.SetDegree(1.0/cds.Value()*180.0);
+    while (LCD.Touch(&x, &y))
+    {
+        arm_servo.SetDegree(1.0 / cds.Value() * 180.0);
     }
 }
-
-void toTray(){
+ 
+void toTray()
+{
     start();
-    move_straight(20, COUNTS_PER_INCH*13);
-    turn_left(20, COUNTS_PER_INCH*5.4);
-    move_straight(20, COUNTS_PER_INCH*19.5);
-    turn_right(20, COUNTS_PER_INCH*4.7);
+    move_straight(20, COUNTS_PER_INCH * 13);
+    turn_left(20, COUNTS_PER_INCH * 5.475);
+    move_straight(20, COUNTS_PER_INCH * 19);
+    turn_right(20, COUNTS_PER_INCH * 5.1);
+    move_straight(20, COUNTS_PER_INCH * 1);
     Sleep(1.0);
 }
-
-void atTray(){
+ 
+void atTray()
+{
     arm_servo.SetMax(SERVO_MAX);
     arm_servo.SetMin(SERVO_MIN);
-    for (float degree = 30.0; degree < 165.0; degree += 5){
+    for (float degree = 30.0; degree < 165.0; degree += 5)
+    {
         arm_servo.SetDegree(degree);
         Sleep(.05);
     }
     Sleep(1.0);
-    move_straight(-20, COUNTS_PER_INCH * 6);
+    move_straight(-20, COUNTS_PER_INCH * 2);
+    arm_servo.SetDegree(155);
+    move_straight(-20, COUNTS_PER_INCH * 2.25);
 }
-
-void toTicket(){
+ 
+void toTicket()
+{
     arm_servo.SetDegree(20.0);
-    turn_right(20, COUNTS_PER_INCH * 5.35);
-    move_straight(20, COUNTS_PER_INCH * 22);
-    turn_left(20, COUNTS_PER_INCH * 4.2);
+    turn_right(20, COUNTS_PER_INCH * 5.8);
+    move_straight(20, COUNTS_PER_INCH * 21.8);
+    turn_left(20, COUNTS_PER_INCH * 4.55);
 }
-
-void atTicket(){
+ 
+void atTicket()
+{
     arm_servo.SetMin(SERVO_MIN);
     arm_servo.SetMax(SERVO_MAX);
     arm_servo.SetDegree(180.0);
-    move_straight(20, COUNTS_PER_INCH * 3.5);
-    Sleep(5.0);
+    move_straight(20, COUNTS_PER_INCH * 6.85);
+    Sleep(1.0);
     angled_turn(5, 20, 50, 2);
 }
-
+ 
+void toHotPlate()
+{
+    arm_servo.SetMin(SERVO_MIN);
+    arm_servo.SetMax(SERVO_MAX);
+ 
+    turn_right(20, COUNTS_PER_INCH * 0.9); // 15 deg turn
+    move_straight(-20, COUNTS_PER_INCH * 4);
+    arm_servo.SetDegree(140);
+    turn_left(20, COUNTS_PER_INCH * NINETY_DEG_TWENTYSPEED);
+    move_straight(20, COUNTS_PER_INCH * 11);
+    turn_right(20, COUNTS_PER_INCH * NINETY_DEG_TWENTYSPEED);
+    move_straight(35, COUNTS_PER_INCH * 20);
+    turn_right(20, COUNTS_PER_INCH * 1.5); // 30 degree turn.
+    move_straight(20, COUNTS_PER_INCH * 24);
+}
+ 
 int main(void)
 {
-    
+ 
     // /* Code to go to the jukebox light from the start*/
     // start();
     // Sleep(1.0);
@@ -283,7 +331,10 @@ int main(void)
     atTray();
     toTicket();
     atTicket();
+    toHotPlate();
+ 
     return 0;
 }
-
+ 
+ 
 
